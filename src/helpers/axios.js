a038c48f959a41630bc7566ad58146d35eaa37b2
Luis Tapia <luistapialml@gmail.com>
2023-09-30 02:04:05 -0600

    se agrega cros anywhere url a url base

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 const instance = axios.create({
     
-    // baseURL: "http://localhost:8080/https://api.deezer.com"
-    baseURL: "https://api.deezer.com"
+    baseURL: "http://localhost:9001/https://api.deezer.com",
+    // baseURL: "https://api.deezer.com",
+    // headers: {
+    //             "Access-Control-Allow-Origin": "*",
+    //             // 'origin': 'x-requested-with'
+    //         },
+    withCredentials:true
 
 });
 // console.log(process.env.BASE_URL);
