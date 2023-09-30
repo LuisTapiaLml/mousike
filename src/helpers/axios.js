3cb2b43dae1552d11a05168fe00321c469865516
Luis Tapia <luistapialml@gmail.com>
2023-09-30 02:33:49 -0600

    se cambia a cors.luisptapia.com

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     
-    baseURL: "http:cors.luisptapia.com/https://api.deezer.com",
+    baseURL: "http://cors.luisptapia.com/https://api.deezer.com",
     // baseURL: "https://api.deezer.com",
     // headers: {
     //             "Access-Control-Allow-Origin": "*",
